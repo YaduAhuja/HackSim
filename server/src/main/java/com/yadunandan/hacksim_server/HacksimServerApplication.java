5c1f18310ce66609948fbe3a9ef6547ab603947c
Yadunandan Ahuja <yaduahuja24@gmail.com>
2025-04-13 17:36:05 +0530

    Initialized Server Project

---
+++ server/src/main/java/com/yadunandan/hacksim_server/HacksimServerApplication.java
@@ -0,0 +1,13 @@
+package com.yadunandan.hacksim_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacksimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacksimServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/yadunandan/hacksim_server/HacksimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yadunandan.hacksim_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacksimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
